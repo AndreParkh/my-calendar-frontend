03b53ecad9d43baa8a76f99f3cbfd75f40e845a8
AndreParkh <andreybmx95@mail.ru>
2025-08-01 21:33:12 +0300

    formated

--- src/store/reducers/authThunks.ts
+++ src/store/reducers/authThunks.ts
@@ -7,26 +7,25 @@ import { redirect } from 'react-router'
 
 const api = import.meta.env.VITE_API_DOMAIN
 
-export const login = createAsyncThunk<
-  string,
-  ILogin,
-  { rejectValue: string }
->('auth/login', async ( credentials , thunkAPI) => {
-  try {
-    const response = await axios.post<LoginResponse>(
-      `${api}/auth/login`,
-      credentials,
-      {
-        headers: {
-          'Content-Type': 'application/json',
+export const login = createAsyncThunk<string, ILogin, { rejectValue: string }>(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post<LoginResponse>(
+        `${api}/auth/login`,
+        credentials,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    )
-    const token = response.data.token
-    Cookies.set(AUTH_TOKEN, token)
-    redirect('app/user')
-    return token
-  } catch {
-    return thunkAPI.rejectWithValue('Неуспешная авторизация')
-  }
-})
+      )
+      const token = response.data.token
+      Cookies.set(AUTH_TOKEN, token)
+      redirect('app/user')
+      return token
+    } catch {
+      return thunkAPI.rejectWithValue('Неуспешная авторизация')
+    }
+  },
+)
