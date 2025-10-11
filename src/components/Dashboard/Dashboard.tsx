e880dae3c88d0dc83302bd7253c169877e7b74d8
AndreParkh <andreybmx95@mail.ru>
2025-10-11 11:42:45 +0300

    added scroll

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -8,9 +8,9 @@ export const Dashboard = () => {
       <div className={styles.sidebar}>
         <CalendarWidget />
       </div>
-      {/*<div className={styles.content}>*/}
-      <WeekDashboard />
-      {/*</div>*/}
+      <div className={styles.content}>
+        <WeekDashboard />
+      </div>
     </div>
   )
 }
